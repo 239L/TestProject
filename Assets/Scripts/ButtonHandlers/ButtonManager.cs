9c064a1147d65c1a3bd9f151508bcd36a5729e64
239L <lisedzerdi@gmail.com>
2024-04-25 23:25:09 +0700

    1

---
+++ Assets/Scripts/ButtonHandlers/ButtonManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager :MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void Enable(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+    public void Disable(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+    public void DestroyObject(GameObject obj)
+    {
+        Destroy(obj);
+    }
+}
